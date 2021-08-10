b6b067643e1be5cd0c192d305758281fb6512ce2
catherine-minaieva <75452152+catherine-minaieva@users.noreply.github.com>
2021-08-10 19:12:24 +0300

    InitDB fixed

--- initDB.sql
+++ initDB.sql
@@ -5,68 +5,68 @@ CREATE DATABASE IF NOT EXISTS test;
 use test;
 
 CREATE TABLE `companies` (
-  `idCompany` int NOT NULL,
+  `id` int NOT NULL,
   `CompanyName` varchar(45) NOT NULL,
   `HeadOffice` varchar(45) NOT NULL,
-  PRIMARY KEY (`idCompany`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`)
+);
 
 CREATE TABLE `customers` (
-  `idCustomer` int NOT NULL,
+  `id` int NOT NULL,
   `CustomersName` varchar(45) NOT NULL,
   `TaxCode` varchar(45) NOT NULL,
   `HeadOffice` varchar(45) NOT NULL,
-  PRIMARY KEY (`idCustomer`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`)
+);
 
 CREATE TABLE `developers` (
-  `idDeveloper` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(45) DEFAULT NULL,
   `Age` int DEFAULT NULL,
   `Gender` enum('men','women') NOT NULL,
-   PRIMARY KEY (`idDeveloper`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+   PRIMARY KEY (`id`)
+  );
 
 CREATE TABLE `projects` (
-  `idProject` int NOT NULL,
+  `id` int NOT NULL,
   `ProjectName` varchar(45) NOT NULL,
   `BaseTechnology` varchar(45) NOT NULL,
-  PRIMARY KEY (`idProject`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`)
+);
 
 CREATE TABLE `skills` (
-  `idSkill` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `language` enum('Java','C++','C#','JS') NOT NULL,
-  `Level` enum('Junior','Middle','Senior') NOT NULL,
-  PRIMARY KEY (`idSkill`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Level` enum('Junior','Middle',' Senior') NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 CREATE TABLE `developers_skills` (
-  `idDeveloper` int NOT NULL,
-  `idSkill` int NOT NULL,
-  PRIMARY KEY (`idDeveloper`,`idSkill`),
-  FOREIGN KEY (`idDeveloper`) REFERENCES `developers`(`idDeveloper`),
-  FOREIGN KEY (`idSkill`) REFERENCES `skills`(`idSkill`));
+  `developer_id` int NOT NULL,
+  `skill_id` int NOT NULL,
+  PRIMARY KEY (`developer_id`,`skill_id`),
+  FOREIGN KEY (`developer_id`) REFERENCES developers (`id`),
+  FOREIGN KEY (`skill_id`) REFERENCES skills (`id`));
 
 CREATE TABLE `developers_projects` (
-  `idDeveloper` int NOT NULL,
-  `idProject` int NOT NULL,
-  PRIMARY KEY (`idDeveloper`,`idProject`),
-  FOREIGN KEY (`idDeveloper`) REFERENCES `developers` (`idDeveloper`),
-  FOREIGN KEY (`idProject`) REFERENCES `projects` (`idProject`));
+  `developer_id` int NOT NULL,
+  `project_id` int NOT NULL,
+  PRIMARY KEY (`developer_id`,`project_id`),
+  FOREIGN KEY (`developer_id`) REFERENCES developers (`id`),
+    FOREIGN KEY (`project_id`) REFERENCES projects (`id`));
 
 CREATE TABLE `companies_projects` (
-  `idCompany` int NOT NULL,
-  `idProject` int NOT NULL,
-  PRIMARY KEY (`idCompany`,`idProject`),
-  KEY `idProject` (`idProject`),
-  FOREIGN KEY (`idCompany`) REFERENCES `companies` (`idCompany`),
-  FOREIGN KEY (`idProject`) REFERENCES `projects` (`idProject`));
+  `company_id` int NOT NULL,
+  `project_id` int NOT NULL,
+  PRIMARY KEY (`company_id`,`project_id`),
+  KEY `project_id` (`project_id`),
+  FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`),
+  FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`));
 
 CREATE TABLE `customers_projects` (
-  `idCustomer` int NOT NULL,
-  `idProject` int NOT NULL,
-  PRIMARY KEY (`idCustomer`,`idProject`),
-  KEY `idProject` (`idProject`),
-  FOREIGN KEY (`idCustomer`) REFERENCES `customers` (`idCustomer`),
-  FOREIGN KEY (`idProject`) REFERENCES `projects` (`idProject`));
+  `customer_id` int NOT NULL,
+  `project_id` int NOT NULL,
+  PRIMARY KEY (`customer_id`,`project_id`),
+  KEY `project_id` (`project_id`),
+  FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`),
+  FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`));

c97e6f4820ff9caba1c3fd0dd5ca2212cfe396cc
catherine-minaieva <75452152+catherine-minaieva@users.noreply.github.com>
2021-08-11 11:21:01 +0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,13 +1,13 @@
 use test;
 alter table developers add column salary double;
-UPDATE developers SET salary = 5000 WHERE (idDeveloper = 1);
-UPDATE developers SET salary = 2600 WHERE (idDeveloper = 2);
-UPDATE developers SET salary = 1200 WHERE (idDeveloper = 3);
-UPDATE developers SET salary = 3200 WHERE (idDeveloper = 4);
-UPDATE developers SET salary = 2400 WHERE (idDeveloper = 5);
-UPDATE developers SET salary = 2500 WHERE (idDeveloper = 6);
-UPDATE developers SET salary = 3300 WHERE (idDeveloper = 7);
-UPDATE developers SET salary = 4400 WHERE (idDeveloper = 8);
-UPDATE developers SET salary = 2600 WHERE (idDeveloper = 9);
-UPDATE developers SET salary = 1000 WHERE (idDeveloper = 10);
+UPDATE developers SET salary = 5000 WHERE (id = 1);
+UPDATE developers SET salary = 2600 WHERE (id = 2);
+UPDATE developers SET salary = 1200 WHERE (id = 3);
+UPDATE developers SET salary = 3200 WHERE (id = 4);
+UPDATE developers SET salary = 2400 WHERE (id = 5);
+UPDATE developers SET salary = 2500 WHERE (id = 6);
+UPDATE developers SET salary = 3300 WHERE (id = 7);
+UPDATE developers SET salary = 4400 WHERE (id = 8);
+UPDATE developers SET salary = 2600 WHERE (id = 9);
+UPDATE developers SET salary = 1000 WHERE (id = 10);
 
